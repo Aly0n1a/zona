f4e4b0e406469df61d52dee3707d7e1596e264e1
Чмирёва Алёна <meloman.ytxd@gmail.com>
2024-12-09 18:30:32 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Zona from './components/Zona/Zona'
 import './App.css'
+import Ps5 from './components/Ps5/Ps5'
 const App = () => {
   return (
     <>
     <Zona/>
+    <Ps5/>
     </>
   )
 }
