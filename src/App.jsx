81ee83a3db54c7a364deecae668057dc43bcb4f7
Чмирёва Алёна <meloman.ytxd@gmail.com>
2024-12-09 14:30:54 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Zona from './components/Zona/Zona'
+import './App.css'
+const App = () => {
+  return (
+    <>
+    <Zona/>
+    </>
+  )
+}
+
+export default App
